599dd099a577770c793d5f161d5d642ead1d3858
Bas Voss <bla.voss@student.avans.nl>
2023-11-16 13:53:38 +0100

    addition to project.json for ci/cd

--- libs/cswdi-web-app/features/src/lib/user/user.service.ts
+++ libs/cswdi-web-app/features/src/lib/user/user.service.ts
@@ -65,7 +65,7 @@ export class UserService {
      * Handle errors.
      */
     public handleError(error: HttpErrorResponse): Observable<any> {
-        console.log('handleError in MealService', error);
+        console.log('handleError in UserService', error);
 
         return throwError(() => new Error(error.message));
     }
