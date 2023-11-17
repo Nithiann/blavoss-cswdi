9a89fdb1476fa5feae5a020c3362d3e7a8eff7a8
Bas Voss <bla.voss@student.avans.nl>
2023-11-17 16:55:43 +0100

    about page opstelling

--- libs/cswdi-web-app/common/src/lib/about/about.component.ts
+++ libs/cswdi-web-app/common/src/lib/about/about.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'blavoss-cswdi-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  imagepath?: string;
+
+  ngOnInit(): void {
+    this.imagepath = 'assets/ERD-casus.jpg';
+  }
+}
