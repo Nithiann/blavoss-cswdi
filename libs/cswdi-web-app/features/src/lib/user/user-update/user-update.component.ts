f220e7b8a6b4d33b3661cdb4b67dfb7fd6bd5c45
Bas Voss <bla.voss@student.avans.nl>
2023-11-17 12:09:06 +0100

    update page frontend

--- libs/cswdi-web-app/features/src/lib/user/user-update/user-update.component.ts
+++ libs/cswdi-web-app/features/src/lib/user/user-update/user-update.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Gender } from '@blavoss-cswdi/shared/api';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'blavoss-cswdi-user-update',
   templateUrl: './user-update.component.html',
   styleUrls: ['./user-update.component.css'],
 })
-export class UserUpdateComponent {}
+export class UserUpdateComponent implements OnInit, OnDestroy {
+
+  updateForm: FormGroup;
+  genderEnum = Object.values(Gender);
+  subscription: Subscription | undefined = undefined;
+
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private route: ActivatedRoute) {
+    this.updateForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const userId = params['id'];
+
+      this.subscription = this.userService.read(userId).subscribe((results: any) => {
+        console.log(`results: ${results}`);
+        this.updateForm?.patchValue({
+          email: results.email,
+          firstName: results.firstName,
+          lastName: results.lastName,
+          gender: results.gender
+        });
+      })
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+
+  onSubmit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
--- libs/cswdi-web-app/features/src/lib/users.module.ts
+++ libs/cswdi-web-app/features/src/lib/users.module.ts
@@ -5,12 +5,13 @@ import { UserDetailComponent } from "./user/user-detail/user-detail.component";
 import { UserService } from "./user/user.service";
 import { UserListComponent } from "./user/user-list/user-list.component";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms'; 
 import { routes } from './user/user-routing.module';
 import { UserUpdateComponent } from "./user/user-update/user-update.component";
 import { UserCreateComponent } from "./user/user-create/user-create.component";
 
 @NgModule({
-    imports: [CommonModule, HttpClientModule, RouterModule.forChild(routes)],
+    imports: [CommonModule, HttpClientModule, RouterModule.forChild(routes), ReactiveFormsModule],
     declarations: [UserDetailComponent, UserListComponent, UserUpdateComponent, UserCreateComponent],
     providers: [UserService],
     exports: [UserDetailComponent, UserListComponent, RouterModule, UserUpdateComponent, UserCreateComponent],
