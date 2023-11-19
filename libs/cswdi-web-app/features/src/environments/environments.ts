51366daf21f8561ecb0de1ea3acae9f834e97c97
Bas Voss <bla.voss@student.avans.nl>
2023-11-19 18:33:53 +0100

    change to environment

--- libs/cswdi-web-app/features/src/environments/environments.ts
+++ libs/cswdi-web-app/features/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://blavoss-cswdi-api.azurewebsites.net/api', // Your API URL for development
+    apiUrl: 'https://cswdi-blavoss-api.azurewebsites.net/api', // Your API URL for development
   };
