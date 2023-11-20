993d84974cf822c8449d70fdf778ba94cf7dc152
Bas Voss <bla.voss@student.avans.nl>
2023-11-20 16:18:32 +0100

    artist detail page

--- libs/cswdi-web-app/features/src/lib/artist/artist-detail/artist-detail.component.ts
+++ libs/cswdi-web-app/features/src/lib/artist/artist-detail/artist-detail.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IArtist } from '@blavoss-cswdi/shared/api';
+import { Subscription } from 'rxjs';
+import { ArtistService } from '../artist.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'blavoss-cswdi-artist-detail',
   templateUrl: './artist-detail.component.html',
   styleUrls: ['./artist-detail.component.css'],
 })
-export class ArtistDetailComponent {}
+export class ArtistDetailComponent implements OnInit, OnDestroy {
+
+  artist: IArtist | null = null;
+  subscription: Subscription | undefined = undefined;
+
+  constructor(private artistService: ArtistService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      const artistId = params['id'];
+
+      this.subscription = this.artistService.read(artistId).subscribe((results: any) => {
+        console.log(`results: ${results}`);
+        this.artist = results;
+      })
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
