c714e72078d4137306bcd11693ad9676347e5e5a
Bas Voss <bla.voss@student.avans.nl>
2023-11-20 16:12:28 +0100

    artist list and create page

--- libs/cswdi-web-app/features/src/lib/artist/artist-list/artist-list.component.ts
+++ libs/cswdi-web-app/features/src/lib/artist/artist-list/artist-list.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IArtist } from '@blavoss-cswdi/shared/api';
+import { Subscription } from 'rxjs';
+import { ArtistService } from '../artist.service';
 
 @Component({
   selector: 'blavoss-cswdi-artist-list',
   templateUrl: './artist-list.component.html',
   styleUrls: ['./artist-list.component.css'],
 })
-export class ArtistListComponent {}
+export class ArtistListComponent implements OnInit, OnDestroy {
+  artists: IArtist[] | null = null;
+  subscription: Subscription | undefined = undefined;
+
+  constructor(private artistService: ArtistService) {}
+
+  ngOnInit(): void {
+      this.subscription = this.artistService.list().subscribe((result) => {
+        this.artists = result;
+      })
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
