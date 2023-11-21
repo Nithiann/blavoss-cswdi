73afa474f092522dbf8ddbe057abd21c40065902
Bas Voss <bla.voss@student.avans.nl>
2023-11-21 16:59:40 +0100

    all works for festivals and artists

--- libs/backend/features/src/lib/festival.module.ts
+++ libs/backend/features/src/lib/festival.module.ts
@@ -1,4 +1,4 @@
-import { DataAccessModule, FestivalSchema } from '@blavoss-cswdi/backend/data-access';
+import { ArtistSchema, DataAccessModule, FestivalSchema } from '@blavoss-cswdi/backend/data-access';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FestivalController } from './festival/festival.controller';
@@ -6,7 +6,7 @@ import { FestivalService } from './festival/festival.service';
 import { ArtistService } from './artist/artist.service';
 
 @Module({
-    imports: [DataAccessModule, MongooseModule.forFeature([{name: 'Festival', schema: FestivalSchema}])],
+    imports: [DataAccessModule, MongooseModule.forFeature([{name: 'Festival', schema: FestivalSchema}]), MongooseModule.forFeature([{ name: 'Artist', schema: ArtistSchema }])],
     controllers: [FestivalController],
     providers: [FestivalService, ArtistService],
     exports: [],
--- libs/cswdi-web-app/features/src/lib/festival/festival-detail/festival-detail.component.ts
+++ libs/cswdi-web-app/features/src/lib/festival/festival-detail/festival-detail.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FestivalService } from '../festival.service';
+import { ActivatedRoute } from '@angular/router';
+import { IFestival } from '@blavoss-cswdi/shared/api';
 
 @Component({
   selector: 'blavoss-cswdi-festival-detail',
   templateUrl: './festival-detail.component.html',
   styleUrls: ['./festival-detail.component.css'],
 })
-export class FestivalDetailComponent {}
+export class FestivalDetailComponent implements OnInit, OnDestroy {
+
+  festival: IFestival | null = null;
+  subscription: Subscription | undefined  = undefined;
+
+  constructor(private festivalService: FestivalService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      const festivalId = params['id'];
+
+      this.subscription = this.festivalService.read(festivalId).subscribe((results: any) => {
+        console.log(`results: ${results}`);
+        this.festival = results;
+      })
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
+
