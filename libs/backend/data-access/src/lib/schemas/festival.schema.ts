74cd0e49440de230812f5810aed81d26ca98bba1
Bas Voss <bla.voss@student.avans.nl>
2023-11-21 13:10:25 +0100

    schema for festival

---
+++ libs/backend/data-access/src/lib/schemas/festival.schema.ts
@@ -0,0 +1,38 @@
+import { Genre } from '@blavoss-cswdi/shared/api';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+import { Artist } from './artist.schema';
+
+export type FestivalDocument = HydratedDocument<Festival>;
+
+@Schema()
+export class Festival {
+    @Prop()
+    name!: string;
+
+    @Prop()
+    location!: string;
+
+    @Prop()
+    startDate!: Date;
+
+    @Prop()
+    endDate!: Date;
+
+    @Prop()
+    description!: string;
+
+    @Prop()
+    ticketPrice!: number;
+
+    @Prop()
+    image!: string;
+
+    @Prop({ type: String, enum: Object.values(Genre), default: Genre.EDM})
+    genre!: Genre;
+
+    @Prop({ type: [Types.ObjectId], ref: 'Artist', default: [] })
+    artists!: Artist[]; //change to festials once object is created
+}
+
+export const FestivalSchema = SchemaFactory.createForClass(Festival);
