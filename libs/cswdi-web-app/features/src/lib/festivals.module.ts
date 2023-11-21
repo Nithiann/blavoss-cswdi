c3753ef2b7ec4ccf0fa7be924f79192e13417720
Bas Voss <bla.voss@student.avans.nl>
2023-11-21 14:41:12 +0100

    fixed import on module

--- libs/cswdi-web-app/features/src/lib/festivals.module.ts
+++ libs/cswdi-web-app/features/src/lib/festivals.module.ts
@@ -7,6 +7,7 @@ import { routes } from "./festival/festival-routing.module";
 import { FestivalListComponent } from "./festival/festival-list/festival-list.component";
 import { FestivalCreateComponent } from "./festival/festival-create/festival-create.component";
 import { FestivalDetailComponent } from "./festival/festival-detail/festival-detail.component";
+import { FestivalService } from "./festival/festival.service";
 
 @NgModule({
     imports: [CommonModule, HttpClientModule, ReactiveFormsModule, RouterModule.forChild(routes)],
