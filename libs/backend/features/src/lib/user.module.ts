15ad71dee69ba45664fb43a22a03f2d1b24e0156
Bas Voss <bla.voss@student.avans.nl>
2023-11-22 13:27:07 +0100

    passport module added

--- libs/backend/features/src/lib/user.module.ts
+++ libs/backend/features/src/lib/user.module.ts
@@ -4,10 +4,12 @@ import { UserService } from './user/user.service';
 import { DataAccessModule, UserSchema } from '@blavoss-cswdi/backend/data-access'; 
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [DataAccessModule, 
     MongooseModule.forFeature([{name: 'User', schema: UserSchema}]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env['JWT_SECRET'],
       signOptions: { expiresIn: '1d' },
