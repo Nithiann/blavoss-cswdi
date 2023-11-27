63251e1085414a52b44d967cdf20058666239b7f
Bas Voss <bla.voss@student.avans.nl>
2023-11-27 14:09:30 +0100

    remove standalone feature on components

--- libs/cswdi-web-app/features/src/lib/ticket/ticket-pay/ticket-pay.component.ts
+++ libs/cswdi-web-app/features/src/lib/ticket/ticket-pay/ticket-pay.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'blavoss-cswdi-ticket-pay',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './ticket-pay.component.html',
   styleUrls: ['./ticket-pay.component.css'],
 })
