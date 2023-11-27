a6b451bd806a04f9edac509e0ff378fce95a38db
Bas Voss <bla.voss@student.avans.nl>
2023-11-27 12:21:19 +0100

    ticket backend controller

--- libs/backend/features/src/lib/ticket/ticket.controller.ts
+++ libs/backend/features/src/lib/ticket/ticket.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { GenericAuthGuard } from '../generic-auth.guard';
+import { TicketService } from './ticket.service';
+import { ITicket } from '@blavoss-cswdi/shared/api';
+import { CreateTicketDTO } from '@blavoss-cswdi/backend/dto';
 
 @Controller('ticket')
-export class TicketController {}
+export class TicketController {
+
+    TAG = 'TicketController';
+
+    constructor(private ticketService: TicketService) {}
+
+    @Get('')
+    @UseGuards(GenericAuthGuard)
+    async getAll(): Promise<ITicket[]> {
+        return await this.ticketService.getAll();
+    }
+
+    @Get(':id')
+    @UseGuards(GenericAuthGuard)
+    async getOne(@Param('id') id: string): Promise<ITicket> {
+        return await this.ticketService.getOne(id);
+    }
+
+    @Put(':id')
+    @UseGuards(GenericAuthGuard)
+    async update(@Param('id') id: string, @Body() updateTicket: Partial<ITicket>): Promise<ITicket> {
+        return await this.ticketService.update(id, updateTicket);
+    }
+
+    @Post('')
+    @UseGuards(GenericAuthGuard)
+    async create(@Body() data: CreateTicketDTO): Promise<ITicket> {
+        return await this.ticketService.create(data);
+    }
+
+    @Delete(':id')
+    @UseGuards(GenericAuthGuard)
+    async delete(@Param('id') id: string): Promise<ITicket> {
+        return await this.ticketService.delete(id);
+    }
+}
