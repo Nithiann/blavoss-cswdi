666fa1d907b8312dc1bec4e51ac84003d1544602
Bas Voss <bla.voss@student.avans.nl>
2023-11-29 12:13:01 +0100

    basic frontend for ticket

--- libs/cswdi-web-app/features/src/lib/ticket/ticket-listing/ticket-listing.component.ts
+++ libs/cswdi-web-app/features/src/lib/ticket/ticket-listing/ticket-listing.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IFestival } from '@blavoss-cswdi/shared/api';
+import { Subscription } from 'rxjs';
+import { FestivalService } from '../../festival/festival.service';
 
 @Component({
   selector: 'blavoss-cswdi-ticket-listing',
   templateUrl: './ticket-listing.component.html',
   styleUrls: ['./ticket-listing.component.css'],
 })
-export class TicketListingComponent {}
+export class TicketListingComponent implements OnInit, OnDestroy{
+
+  festivals: IFestival[] | null = null;
+  subscription: Subscription | undefined = undefined;
+  constructor(private festivalService: FestivalService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.festivalService.list().subscribe((festivals) => {
+      this.festivals = festivals;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
--- libs/cswdi-web-app/features/src/lib/ticket/ticket-pay/ticket-pay.component.ts
+++ libs/cswdi-web-app/features/src/lib/ticket/ticket-pay/ticket-pay.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '@blavoss-cswdi/common';
+import { IFestival, ITicket, IUser } from '@blavoss-cswdi/shared/api';
+import { Subscription } from 'rxjs';
+import { FestivalService } from '../../festival/festival.service';
 
 @Component({
   selector: 'blavoss-cswdi-ticket-pay',
   templateUrl: './ticket-pay.component.html',
   styleUrls: ['./ticket-pay.component.css'],
 })
-export class TicketPayComponent {}
+export class TicketPayComponent implements OnInit, OnDestroy {
+
+  festival: IFestival | null = null;
+  user: IUser | null = null;
+  subscription: Subscription | undefined = undefined;
+  ticket: ITicket | null = null;
+  constructor(private authService: AuthService, private festivalService: FestivalService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.getDecodedToken().subscribe((user) => {
+      this.user = user;
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe();
+  }
+}
