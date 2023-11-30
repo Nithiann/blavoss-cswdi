21fe663c4c7b5a4c29b9943d74beed756f9ebe1e
Bas Voss <bla.voss@student.avans.nl>
2023-11-30 14:12:57 +0100

    neo4j data access coupling

--- libs/backend/data-access/src/lib/neo-data-access.module.ts
+++ libs/backend/data-access/src/lib/neo-data-access.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { Neo4jModule } from './nodes/neo4js.module';
 
 @Module({
   imports: [
-    
+    Neo4jModule.forRoot()
   ],
   controllers: [],
   providers: [],
--- libs/backend/data-access/src/lib/nodes/neo4js.module.ts
+++ libs/backend/data-access/src/lib/nodes/neo4js.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { Neo4jService } from './neo4j.service';
+import * as neo4j from 'neo4j-driver';
 
 @Module({
   providers: [Neo4jService],
   exports: [Neo4jService],
 })
-export class Neo4jModule {}
+export class Neo4jModule {
+    static forRoot() {
+        return {
+          module: Neo4jModule,
+          providers: [
+            {
+              provide: 'NEO4J',
+              useFactory: () => {
+                return neo4j.driver(
+                    `${process.env['NEO4J_SCHEME']}://${process.env['NEO4J_HOST'] } ` ?? 'bolt://localhost:7687',
+                    neo4j.auth.basic(
+                        process.env!['NEO4J_USER'] ?? 'neo4j',
+                        process.env!['NEO4J_PASSWORD'] ?? 'password'
+                    ),
+                );
+              },
+            },
+          ],
+        };
+      }
+}
