6bad59354226a700a4736fffa34a17785181f594
Bas Voss <bla.voss@student.avans.nl>
2023-11-30 14:13:54 +0100

    added to rmcnd-api app

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NeoDataAccessModule } from '@blavoss-cswdi/backend/data-access';
 
 @Module({
-  imports: [],
+  imports: [
+    NeoDataAccessModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
