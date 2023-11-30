5ddc100a0b2e3c69ffb9a8f0e65d100cefaad2a3
Bas Voss <bla.voss@student.avans.nl>
2023-11-30 13:55:50 +0100

    neo module

--- libs/backend/data-access/src/index.ts
+++ libs/backend/data-access/src/index.ts
@@ -3,3 +3,9 @@ export * from './lib/schemas/user.schema';
 export * from './lib/schemas/artist.schema';
 export * from './lib/schemas/festival.schema';
 export * from  './lib/schemas/ticket.schema';
+
+export * from './lib/neo-data-access.module';
+export * from './lib/nodes/user.node';
+export * from './lib/nodes/artist.node';
+export * from './lib/nodes/festival.node';
+export * from './lib/nodes/ticket.node';
